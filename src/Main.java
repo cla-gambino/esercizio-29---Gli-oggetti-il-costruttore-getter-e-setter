039c9323f37aa9935d2b0d409e59e582318f7b45
cla-gambino <154328607+cla-gambino@users.noreply.github.com>
2024-01-16 18:08:36 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Studente student1 = new Studente("Claudio", "Gambino", "#001");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Studente student2 = new Studente("Pinco", "Panco", "#002");
+
+        Studente student3 = new Studente("Panco", "Pinco", "#003");
+
+        //student1.setFirstName("Nicola");     // possiamo modificare ad esempio il nome del primo studente grazie al metodo setters
+
+        Classe classe = new Classe();
+        classe.studenti = new Studente[]{student1, student2, student3};
+
+        System.out.println("classe: " + classe);
+
     }
 }
